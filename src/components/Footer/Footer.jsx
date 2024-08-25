2c52e7e9ba0a5c0d7e8b5ad65dca4003db9fa9e9
Dharaneeswara <dharaneeswar7@gmail.com>
2024-08-25 13:06:00 +0530

    add: initial files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, vitae quod? Dicta, ut non? Eos, nam voluptatibus. Dolore odit aut quasi ratione, inventore eligendi! Iste, error voluptatibus? Saepe, ullam accusantium.</p>
+                <div className="footer-social-icon">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy police</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className='footer-copyright'> Copyright &#016; 2024 Dharaneeswaran V All Rights Reserved </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contants">
+            <h2>Order Your favouret food here</h2>
+            <p>Choose from a Diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinery expertise. One delicious meal at a time</p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
